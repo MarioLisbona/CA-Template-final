7deaffa6eff4bc16b2f99501bb1d9ecd8b328624
Mario Lisbona <mario.lisbona@gmail.com>
2023-01-23 07:01:17 +1100

    change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
 	return (
-		<div class="p-5 my-4 bg-light rounded-3">
-				<h1>Welcome to the Traveler's Forum</h1>
+		<div class="p-5 my-4 bg-dark rounded-3 text-white">
+				<h1>Welcome to the staging environment</h1>
 				<p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis eos quam autem ipsa non placeat amet reiciendis incidunt fugit iure.. At <a href="https://www.7travelerforum.com.au" class="text-success" target="_blank">travelerforum.com</a> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veritatis vitae, voluptate est deleniti ratione! connect with the people around the world.</p>
 				<p><a href="https://www.tutorialrepublic.com" target="_blank" class="btn btn-success btn-lg">Register Today</a></p>
 		</div>
